047a092a58e3d7c3482ef69b70f206042f3a2207
xudi <751615606@qq.com>
2022-02-25 11:39:56 +0800

    idea-first-commit

---
+++ src/main/java/com/xudi/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xudi.git;
+
+/**
+ * @author Edison
+ * @createTime 2022-02-25 3:41
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("git-test");
+    }
+}
